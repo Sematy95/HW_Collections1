0cc691344249753e6bc1471cc28c0426c836d737
tychk <semarooney@yandex.ru>
2024-08-10 23:27:23 +0300

    HW_Collections

---
+++ src/test/java/pro/sky/skyprospringdemo/Hw23ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
