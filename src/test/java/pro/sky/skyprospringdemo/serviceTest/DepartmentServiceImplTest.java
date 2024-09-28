10024f854bd7c638b2c796e6dfef5bce4fc347bc
tychk <semarooney@yandex.ru>
2024-09-29 02:21:34 +0300

    Mockito testing in progress 2

--- src/test/java/pro/sky/skyprospringdemo/serviceTest/DepartmentServiceImplTest.java
+++ src/test/java/pro/sky/skyprospringdemo/serviceTest/DepartmentServiceImplTest.java
@@ -3,27 +3,32 @@ package pro.sky.skyprospringdemo.serviceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.stubbing.OngoingStubbing;
 import pro.sky.skyprospringdemo.domain.Employee;
+import pro.sky.skyprospringdemo.exceptions.EmployeeNotFoundException;
+import pro.sky.skyprospringdemo.exceptions.NoEmployeesInDepartmentException;
 import pro.sky.skyprospringdemo.repository.EmployeeRepository;
 import pro.sky.skyprospringdemo.service.DepartmentService;
 import pro.sky.skyprospringdemo.service.DepartmentServiceImpl;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @ExtendWith(MockitoExtension.class)
 public class DepartmentServiceImplTest {
 
-    private static final  Map<String, Employee> employees = new HashMap<>(Map.of(
+    private static final Map<String, Employee> employees = new HashMap<>(Map.of(
 
             "IvanIvanov", new Employee("Ivan", "Ivanov", 1, 200_000),
             "AlexanderPetrov", new Employee("Alexander", "Petrov", 1, 100_000),
@@ -44,7 +49,6 @@ public class DepartmentServiceImplTest {
     private DepartmentServiceImpl departmentService;
 
 
-
     @DisplayName("Положительный тест- показать всех сотрудников в отделе")
     @Test
     public void showAllEmployeeDep() {
@@ -53,22 +57,24 @@ public class DepartmentServiceImplTest {
 
         //given
 
-        Collection <Employee> expected = employees.values().stream()
+        Collection<Employee> expected = employees.values().stream()
                 .filter(d -> d.getDepartment() == 1).toList();
 
 
         //when
-        Collection <Employee> actual =departmentService.showAllEmployeeDep(1);
+        Collection<Employee> actual = departmentService.showAllEmployeeDep(1);
 
 
         //then
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
 
 
     }
+
     @DisplayName("Положительный тест - показать всех сотрудников из несуществующего отдела")
     @Test
     public void showAllEmployeeDepNeg() {
+
         when(employeeRepository.getEmployees())
                 .thenReturn(employees);
 
@@ -78,31 +84,105 @@ public class DepartmentServiceImplTest {
 
 
         //when
-        Collection <Employee> actual =departmentService.showAllEmployeeDep(199);
+        Collection<Employee> actual = departmentService.showAllEmployeeDep(199);
+
+        //then
+        assertEquals(expected, actual);
+
+    }
+
+    @DisplayName("Положительный тест - найти сотрудника с минимальной зп в отделе")
+    @Test
+    public void findMinSalaryEmpDep() {
+        when(employeeRepository.getEmployees())
+                .thenReturn(employees);
+        //given
+        Optional<Employee> expected = employees.values().stream()
+                .filter(Objects::nonNull)
+                .filter(d -> d.getDepartment() == 1)
+                .min(Comparator.comparingInt(Employee::getSalary));
+
+        //when
+        Optional<Employee> actual = departmentService.findMinSalaryEmpDep(1);
+
+        //then
+        assertEquals(expected, actual);
+    }
+
+    @DisplayName("Негативный тест - найти сотрудника с минимальной зп в отделе, если отдела нет")
+    @Test
+    public void findMinSalaryEmpDepNoDepartment() {
+        when(employeeRepository.getEmployees())
+                .thenReturn(employees);
+
+        //given//when//then
+        assertThrows(NoEmployeesInDepartmentException.class, () -> departmentService.findMinSalaryEmpDep(100));
+    }
+
+
+    @DisplayName("Положительный тест - найти сотрудника с максимальной зп в отделе")
+    @Test
+    public void findMaxSalaryEmpDep() {
+        when(employeeRepository.getEmployees())
+                .thenReturn(employees);
+        //given
+        Optional<Employee> expected = employees.values().stream()
+                .filter(Objects::nonNull)
+                .filter(d -> d.getDepartment() == 1)
+                .max(Comparator.comparingInt(Employee::getSalary));
+
+        //when
+        Optional<Employee> actual = departmentService.findMaxSalaryEmpDep(1);
+
+        //then
+        assertEquals(expected, actual);
+
+    }
+    @DisplayName("Негативный тест - найти сотрудника с максимальной зп в отделе, если отдела нет")
+    @Test
+    public void findMaxSalaryEmpDepNoDepartment() {
+        when(employeeRepository.getEmployees())
+                .thenReturn(employees);
+
+        //given//when//then
+        assertThrows(NoEmployeesInDepartmentException.class, () -> departmentService.findMaxSalaryEmpDep(100));
+    }
+    @DisplayName("Позитивный тест - сортировка по отделам")
+    @Test
+    public void showAllEmployeeAllDep() {
+        when(employeeRepository.getEmployees())
+                .thenReturn(employees);
+        //given
+        Map<Integer, List<Employee>> expected = employees.values().stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment));
+
+        //when
+        Map<Integer, List<Employee>> actual = departmentService.showAllEmployeeAllDep();
+
+        //then
+        assertEquals(expected, actual);
+    }
+    @DisplayName("Позитивный тест - поиск суммы зп по отделу")
+    @ParameterizedTest
+    @MethodSource("provideDataForShowSalarySumInDepartment")
+    public void showSalarySumInDepartment(int department, int expected) {
+        when(employeeRepository.getEmployees())
+                .thenReturn(employees);
+
+        //when
+        int actual = departmentService.showSalarySumInDepartment(department);
 
         //then
         assertEquals(expected,actual);
 
     }
 
-    @Test
-    public void findMinSalaryEmpDep() {
+    private static Stream<Arguments> provideDataForShowSalarySumInDepartment() {
+        return Stream.of(
+                Arguments.arguments(1, 300_000),
+                Arguments.arguments(4, 250_000));
 
 
-    }
-
-    @Test
-    public void findMaxSalaryEmpDep() {
-
-    }
-
-    @Test
-    public void showAllEmployeeAllDep() {
-
-    }
-
-    @Test
-    public void showSalarySumInDepartment() {
     }
 }
 
--- src/test/java/pro/sky/skyprospringdemo/serviceTest/EmployeeServiceImplTest.java
+++ src/test/java/pro/sky/skyprospringdemo/serviceTest/EmployeeServiceImplTest.java
@@ -1,25 +1,101 @@
 package pro.sky.skyprospringdemo.serviceTest;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import pro.sky.skyprospringdemo.domain.Employee;
-import pro.sky.skyprospringdemo.exceptions.EmployeeAlreadyAddedException;
-import pro.sky.skyprospringdemo.exceptions.EmployeeNotFoundException;
-import pro.sky.skyprospringdemo.exceptions.EmployeeStorageIsFullException;
-import pro.sky.skyprospringdemo.exceptions.InvalidInputException;
+import pro.sky.skyprospringdemo.exceptions.*;
+import pro.sky.skyprospringdemo.repository.EmployeeRepository;
+import pro.sky.skyprospringdemo.service.EmployeeServiceImpl;
+import pro.sky.skyprospringdemo.domain.Employee;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.apache.commons.lang3.StringUtils.isAlpha;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class EmployeeServiceImplTest {
 
+    private static final Map<String, Employee> employees = new HashMap<>(Map.of(
+
+            "IvanIvanov", new Employee("Ivan", "Ivanov", 1, 200_000),
+            "AlexanderPetrov", new Employee("Alexander", "Petrov", 1, 100_000),
+            "MikhailSidorov", new Employee("Mikhail", "Sidorov", 3, 200_000),
+            "StepanProkhorov", new Employee("Stepan", "Prokhorov", 4, 250_000),
+            "LidiaStephanova", new Employee("Lidia", "Stephanova", 5, 92_000),
+            "VictoriaGavrilova", new Employee("Victoria", "Gavrilova", 5, 90_000),
+            "YuliaKarelina", new Employee("Yulia", "Karelina", 3, 220_000),
+            "MikhailVasiliev", new Employee("Mikhail", "Vasiliev", 3, 350_000),
+            "OlegMokrousov", new Employee("Oleg", "Mokrousov", 3, 250_000),
+            "OlegSmagin", new Employee("Oleg", "Smagin", 3, 150_000)
+    ));
+
+    @Mock
+    private EmployeeRepository employeeRepository;
+
+    @InjectMocks
+    private EmployeeServiceImpl employeeService;
+
+    @DisplayName("Негативный тест- хранилище переполнено")
+    @Test
+    public void addEmployeeWithFullStorage() {
+        when(employeeRepository.getEmployees())
+                .thenReturn(employees);
+
+        //employees.put("SemyonTychkin", new Employee("Semyon", "Tychkin", 1, 200_000));
+        employees.put("SemyonTychkin1", new Employee("Semyon", "Tychkin1", 1, 200_000));
+
+
+        //given//when//then
+        assertThrows(EmployeeStorageIsFullException.class, () -> employeeService.addEmployee("Ivan", "Ermenev", 120_000, 3));
+
+    }
+    @DisplayName("Негативный тест- уже есть такой сотрудник")
+    @Test
+    public void addEmployeeAlreadyAdded() {
+        when(employeeRepository.getEmployees())
+                .thenReturn(employees);
+
+
+        //given//when//then
+        assertThrows(EmployeeAlreadyAddedException.class, () -> employeeService.addEmployee("Ivan", "Ivanov", 120_000, 3));
+
+    }
+    @DisplayName("Позитивный тест- добавление нового сотрудника ")
     @Test
     public void addEmployee() {
+        Employee employee = new Employee("Semyon", "Tychkin", 1, 200_000);
+
+        //when(employeeRepository.employees.put(employee.getFullName(), employee)).thenReturn(employees.put(employee.getFullName(), employee));
+
+        //given
+        Map<String, Employee> employees1 = new HashMap<>();
+        employees1.putAll(employees);
+        employees1.put("SemyonTychkin", new Employee("Semyon", "Tychkin", 1, 200_000));
+
+
+        //actual
+        employees.put(employee.getFullName(), employee);
+        //employeeService.addEmployee("Semyon", "Tychkin", 1, 200_000);
+
+        //then
+        assertEquals(employees1, employees);
+
+
+
 
     }
 
-   @Test
+    @Test
     public void findEmployee() {
     }
 
@@ -27,7 +103,7 @@ public class EmployeeServiceImplTest {
     public void deleteEmployee() {
     }
 
-   @Test
+    @Test
     public void showAll() {
     }
 
